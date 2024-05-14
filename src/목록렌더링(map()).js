833e51ca9b699b294ee298aa97a319fdfc4749e0
ggang89 <ggang89@gmail.com>
2024-05-14 17:04:26 +0900

    map(),filter()메서드는 key가 필요하다

--- src/목록렌더링(map()).js
+++ src/목록렌더링(map()).js
@@ -1,12 +1,16 @@
 //map() 메서드
 
-const people = ["kkk", "ppp", "lll", "jjj"];
+const people = [
+  { name: "kkk", id: 1 },
+  { name: "ppp", id: 0 },
+  { name: "lll", id: 2 },
+  { name: "jjj", id: 3 },
+];
 
 export default function List() {
-  const listItems = people.map((p) => <li>{p}</li>);
+  const listItems = people.map((p) => <li key={p.id}>{p.name}</li>);
   return (
     <>
-      
       <h3>목록렌더링 map()함수</h3>
       <ul>{listItems}</ul>
     </>
