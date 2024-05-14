28887acc065de44acc9c55b6b092e95615ba6a38
ggang89 <ggang89@gmail.com>
2024-05-14 16:04:50 +0900

    BuyList

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Button from './Button';
 import { b as Btn2} from './Button2'
+import BuyList from './BuyList';
 //export function b라고 해준 경우, default안해주면 {중괄호} 써야함
 //b컴포넌트를 Btn2로 명명할 수 있다
 import Label from './Label';
@@ -22,6 +23,9 @@ const user2 = {
 function App() {
   //return 안에는 부모요소로 하나 감싸줘야 한다
   //<div></div> 이나 <></>(fragment)쓰기
+  
+    
+  
   return (
     <div>
       <h1>리액트 공식문서 학습하기</h1>
@@ -31,6 +35,8 @@ function App() {
       <Profile user={user1}/>
       <Profile user={user2}/>
       <Pd />
+      <BuyList />
+      
     </div>
   );
 }
